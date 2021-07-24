e88a3d37ba3661399ef4b86be144d4cbbb167bf6
Romario <brishnel@gmail.com>
2021-07-24 11:52:24 -0500

    Login page, Sign up page and drawer implemented.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:my_app/widgets/nav-drawer.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home:MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -47,68 +48,212 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        height:height,
+        width:width,
+        child: SingleChildScrollView(
+         child:Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Container(
+               width: width,
+               height:height*0.35,
+             ),
+             Padding(
+               padding: const EdgeInsets.all(8.0),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Center(
+                   child: Text('Login',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                   ),
+                 ],
+               ),
+             ),
+            SizedBox(height: 30.0,),
+             TextField(
+                decoration: InputDecoration(
+                hintText: 'Email',
+                suffixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                ),
+                ),
+             ),
+    SizedBox(height: 20.0,),
+    TextField(
+    obscureText: true,
+    decoration: InputDecoration(
+    hintText: 'Password',
+    suffixIcon: Icon(Icons.visibility_off),
+    border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(20.0),
+    ),
+    ),
+    ),
+    SizedBox(height: 30.0,),
+    Padding(
+    padding: const EdgeInsets.all(10.0),
+    child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+    Text('Forget password?',style: TextStyle(fontSize: 15.0),),
+    ElevatedButton(onPressed: (){
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>Landing()));
+    }, child: Text('Login'))
+           ],
+      ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+             SizedBox(height:20.0),
+             GestureDetector(
+               onTap: (){
+                 Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
+               },
+               child: Text.rich(
+                 TextSpan(
+                     text: 'Don\'t have an account ',
+                     children: [
+                       TextSpan(
+                         text: 'Signup',
+                         style: TextStyle(
+                             color: Color(0xffEE7B23)
+                         ),
+                       ),
+                     ]
+                 ),
+               ),
+             ),
+
+
+           ],
+         ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: width,
+                height: height*0.45,
+               // child: Image.asset('assets/play.png',fit: BoxFit.fill,),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('Signup',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Forget password?',style: TextStyle(fontSize: 12.0),),
+                    RaisedButton(
+                      child: Text('Signup'),
+                      color: Color(0xffEE7B23),
+                      onPressed: (){},
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height:20.0),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>MyApp()));
+                },
+                child: Text.rich(
+                  TextSpan(
+                      text: 'Already have an account',
+                      children: [
+                        TextSpan(
+                          text: 'Signin',
+                          style: TextStyle(
+                              color: Color(0xffEE7B23)
+                          ),
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Landing extends StatefulWidget {
+  @override
+  _LandingState createState() => _LandingState();
+}
+
+class _LandingState  extends State<Landing>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text('Side menu'),
+      ),
+      body: Center(
+        child: Text('Side Menu Tutorial'),
+      ),
     );
   }
 }
---
+++ lib/widgets/nav-drawer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'Side menu',
+              style: TextStyle(color: Colors.white, fontSize: 25),
+            ),
+            decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+          ),
+          ListTile(
+            leading: Icon(Icons.payments_sharp),
+            title: Text('Payment'),
+            onTap: () => {},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('About'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      ),
+    );
+  }
+}
