89b1ee815421af5d3594992c5620db946dea3271
shemr1 <shemr@live.com>
2021-07-27 18:51:22 -0500

    lets see

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,11 +1,13 @@
 class User{
- late String name;
- late String email;
- late String uid;
- late int phoneNumber;
+  User(this.name , this.email, this.uid, this.phoneNumber, this.balance);
+
+  late String name;
+  late String email ;
+  late String uid ;
+  late int phoneNumber;
  late int balance;
 
-  User(String name, String email, String uid, int phoneNumber, int balance);
+
 
   User.fromJson(Map<String, dynamic> json){
     name = json['Name'];
@@ -15,12 +17,12 @@ class User{
     balance= json['balance'];
   }
 
-  Map<String, dynamic> toJson(){
-    final Map<String,dynamic> data = new Map<String, dynamic>();
-    data ['Name'] = this.name;
-    data ['email'] = this.email;
-    data ['Uid'] = this.uid;
-    data ['phoneNumber'] = this.phoneNumber;
-    data ['balance'] =  this.balance;
-  }
+  // Map<String, dynamic> toJson(){
+  //   final Map<String,dynamic> data = new Map<String, dynamic>();
+  //   data ['Name'] = this.name;
+  //   data ['email'] = this.email;
+  //   data ['Uid'] = this.uid;
+  //   data ['phoneNumber'] = this.phoneNumber;
+  //   data ['balance'] =  this.balance;
+  // }
 }
