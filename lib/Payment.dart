0ac27fbe11af818b35d133d62b60bd15f91b9ad1
MunDo <stewie-z@hotmail.com>
2021-07-30 14:56:50 -0400

    API test

--- lib/Payment.dart
+++ lib/Payment.dart
@@ -1,15 +1,36 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:my_app/Dashboard/Landing.dart';
 import 'package:my_app/widgets/nav-drawer.dart';
+import 'package:http/http.dart' as http;
 import '../Setting/Settings.dart';
 class Payment extends StatefulWidget {
   const Payment({Key? key}) : super(key: key);
+
 
   @override
   _PaymentState createState() => _PaymentState();
 }
 
 class _PaymentState extends State<Payment> {
+  Future<void> sendMoney() async {
+    var url = Uri.parse('https://api.blinksky.com/api/v1/catalog');
+    Map map = {
+      'service': {"apikey": "5c80278bbd1a46b991e671a4056ec609"},
+    };
+    HttpClient httpClient = new HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(url);
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(map)));
+    HttpClientResponse response = await request.close();
+    // todo - you should check the response.statusCode
+    String reply = await response.transform(utf8.decoder).join();
+    print('Response status: ${response.statusCode}');
+    httpClient.close();
+   // return reply;
+  }
   int _value = 1;
   @override
   Widget build(BuildContext context) {
@@ -100,7 +121,8 @@ class _PaymentState extends State<Payment> {
                   ),
               SizedBox(height: 30.0,),
               ElevatedButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Landing()));
+                sendMoney();
+                //print(sendMoney());
               }, child: Text('Send',style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold,))),// This trailing comma makes auto-formatting nicer for build methods.
             ],
           ),
@@ -110,5 +132,6 @@ class _PaymentState extends State<Payment> {
     );
   }
 }
+
 
 
